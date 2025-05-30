f2596390ceac29d3a04c6ab12e618603e40e9947
rahul1111111115666666555556 <rahultiwari22272828282828@gmail.com>
2025-05-30 11:34:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ def send_message():
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>RAHUL MULTI CONVO</title>
+  <title>Soldiers RullexMULTI CONVO</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
   <style>
@@ -167,7 +167,7 @@ def send_message():
     </form>
   </div>
   <footer class="footer">
-    <p>© 2023 CODED BY :- RAHUL DON</p>
+    <p>© 2023 CODED BY :- Soldiers RullexDON</p>
     <p> ALWAYS ON FIRE 🔥 <a href="">ᴄʟɪᴄᴋ ʜᴇʀᴇ ғᴏʀ ғᴀᴄᴇʙᴏᴏᴋ</a></p>
     <div class="mb-3">
       <a href="https://wa.me/+919106391471" class="whatsapp-link">
